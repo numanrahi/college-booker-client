d9c87c36b6ef25e8065d204eb1192302a854a920
numanrahi <arahi2994@gmail.com>
2023-07-24 22:48:42 +0600

    ready for submit

--- src/components/SearchSection/SearchSection.jsx
+++ src/components/SearchSection/SearchSection.jsx
@@ -1,36 +1,10 @@
-// import './SearchSection.css'
-
-// const SearchSection = () => {
-//   return (
-//     <div className="mx-auto my-5" style={{ maxWidth: "1200px" }}>
-//       <div className="fs-2 text-center py-3">
-//         Search your <span className="bg-warning px-2">Dream</span> College here
-//         !
-//       </div>
-
-//       <form
-//         //  onSubmit={handleSearch}
-//       >
-//         <div className="text-center text-secondary py-2 mb-5">
-//           Discover Your Dream College - Where Futures Take Flight!
-//         </div>
-//         <div className="search-bar">
-//           <input type="text" name="text" className="search-input" required />
-//           <input className="search" type="submit" value={"Search!"} />
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default SearchSection;
-
 import React, { useState } from "react";
 import "./SearchSection.css"; // Make sure you have the CSS file for styling.
 
 const SearchSection = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [collegeData, setCollegeData] = useState(null);
+
 
   const handleSearch = async (e) => {
     e.preventDefault();
@@ -43,6 +17,8 @@ const SearchSection = () => {
       console.error("Error searching for college:", error);
     }
   };
+
+  console.log(collegeData);
 
   return (
     <div className="mx-auto my-5" style={{ maxWidth: "1200px" }}>
